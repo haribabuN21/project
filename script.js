c7e5f27508ef368bbee37bf2ff90e383cbde00ee
haribabuN21 <haribabun119@gmail.com>
2022-08-21 12:23:06 +0530

    website

--- script.js
+++ script.js
@@ -37,128 +37,129 @@ fetchdata();
 
 
 
-let names = function searchName(){
-    fetch("https://api.openbrewerydb.org/breweries")
-    .then(Response => {
-        if(!Response.ok){
-            throw Error("ERROR");
-        }
-        return Response.json();
-    }).then(data =>{
-    const html = data.map(user => {
-        return `
+// let names = function searchName(){
+//     fetch("https://api.openbrewerydb.org/breweries")
+//     .then(Response => {
+//         if(!Response.ok){
+//             throw Error("ERROR");
+//         }
+//         return Response.json();
+//     }).then(data =>{
+//     const html = data.map(user => {
+//         return `
      
 
-        <div class="shop">
+//         <div class="shop">
 
-        <h4>Name: ${user.name} </h4>
+//         <h4>Name: ${user.name} </h4>
        
-         </div>
-        `
-    }).join("")
+//          </div>
+//         `
+//     }).join("")
     
 
 
 
 
 
-      document.querySelector('#app').insertAdjacentHTML('afterbegin',html);
-    }).catch(error =>{
-        console.log(error);
-    });
-}
+//       document.querySelector('#app').insertAdjacentHTML('afterbegin',html);
+//     }).catch(error =>{
+//         console.log(error);
+//     });
+// }
 
-let type = function searchType(){
-    fetch("https://api.openbrewerydb.org/breweries")
-    .then(Response => {
-        if(!Response.ok){
-            throw Error("ERROR");
-        }
-        return Response.json();
-    }).then(data =>{
-    const html = data.map(user => {
-        return `
+// let type = function searchType(){
+//     fetch("https://api.openbrewerydb.org/breweries")
+//     .then(Response => {
+//         if(!Response.ok){
+//             throw Error("ERROR");
+//         }
+//         return Response.json();
+//     }).then(data =>{
+//     const html = data.map(user => {
+//         return `
      
 
-        <div class="shop">
+//         <div class="shop">
 
-        <p>Name: ${user.brewery_type} </p>
+//         <p>Name: ${user.brewery_type} </p>
        
-         </div>
-        `
-    }).join("")
+//          </div>
+//         `
+//     }).join("")
     
 
 
 
 
 
-      document.querySelector('#app').insertAdjacentHTML('afterbegin',html);
-    }).catch(error =>{
-        console.log(error);
-    });
-}
+//       document.querySelector('#app').insertAdjacentHTML('afterbegin',html);
+//     }).catch(error =>{
+//         console.log(error);
+//     });
+// }
 
-let addres = function searchType(){
-    fetch("https://api.openbrewerydb.org/breweries")
-    .then(Response => {
-        if(!Response.ok){
-            throw Error("ERROR");
-        }
-        return Response.json();
-    }).then(data =>{
-    const html = data.map(user => {
-        return `
+// let addres = function searchType(){
+//     fetch("https://api.openbrewerydb.org/breweries")
+//     .then(Response => {
+//         if(!Response.ok){
+//             throw Error("ERROR");
+//         }
+//         return Response.json();
+//     }).then(data =>{
+//     const html = data.map(user => {
+//         return `
      
 
-        <div class="shop">
+//         <div class="shop">
 
-        <p>Name: ${user.address_2} </p>
+//         <p>Name: ${user.address_2} </p>
        
-         </div>
-        `
-    }).join("")
+//          </div>
+//         `
+//     }).join("")
     
 
 
 
 
 
-      document.querySelector('#app').insertAdjacentHTML('afterbegin',html);
-    }).catch(error =>{
-        console.log(error);
-    });
-};
+//       document.querySelector('#app').insertAdjacentHTML('afterbegin',html);
+//     }).catch(error =>{
+//         console.log(error);
+//     });
+// };
 
 
-let phone = function searchPhone(){
-    fetch("https://api.openbrewerydb.org/breweries")
-    .then(Response => {
-        if(!Response.ok){
-            throw Error("ERROR");
-        }
-        return Response.json();
-    }).then(data =>{
-    const html = data.map(user => {
-        return `
+// let phone = function searchPhone(){
+//     fetch("https://api.openbrewerydb.org/breweries")
+//     .then(Response => {
+//         if(!Response.ok){
+//             throw Error("ERROR");
+//         }
+//         return Response.json();
+//     }).then(data =>{
+//     const html = data.map(user => {
+//         return `
      
 
-        <div class="shop">
+//         <div class="shop">
 
-        <p>Name: ${user.phone} </p>
+//         <p>Name: ${user.phone} </p>
        
-         </div>
-        `
-    }).join("")
+//          </div>
+//         `
+//     }).join("")
     
 
 
 
 
 
-      document.querySelector('#app').insertAdjacentHTML('afterbegin',html);
-    }).catch(error =>{
-        console.log(error);
-    });
+//       document.querySelector('#app').insertAdjacentHTML('afterbegin',html);
+//     }).catch(error =>{
+//         console.log(error);
+//     });
 
 
+// }
